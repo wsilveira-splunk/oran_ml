dbe39c9c51787ead3bfb70ae3a683914017e046a
Wagner Silveira <wsilveira@splunk>
2021-04-27 15:42:22 -0300

    updated oran model

--- oran_sim/oran_sim.py
+++ oran_sim/oran_sim.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 from collections import OrderedDict
 
+import requests
 import json
 import csv
 import time
@@ -14,35 +15,53 @@ DOCKER_API_VERSION = '1.40'
 COLUMNS = ['TS', 'UEID', 'UETYPE', 'UEPACKETTYPE', 'UEPACKETVALUE', 'ORANSERVICE', 'ORANCPUUSAGE', 'ORANAVGCPUUSAGE', 'ORANSERVICES','ORANMEMUSAGE', 'ORANTXBYTES', 'ORANRXBYTES', 'ORANSCALE']
 MAX_NUMBER_UES = 50
 
+ORAN_CONTROL_CHANNEL_UPT_ATTR = ORAN_BASE_URL + 'control__channel_1/attrs?options=forcedUpdate'
+
+def update_control_channel():
+    payload = {
+        'CPU_CORES': {
+            'type': 'int',
+            'value': 2
+        },
+        'POST_CALLS': {
+            'type': 'int',
+            'value': 0
+        },
+        'DELAY': {
+            'type': 'int',
+            'value': 0
+        },
+        'PACKET_DATA_SIZE': {
+            'type': 'int',
+            'value': 0
+        }
+    }
+
+    response = requests.request("POST",
+                                ORAN_CONTROL_CHANNEL_UPT_ATTR,
+                                headers={'Content-Type': 'application/json'},
+                                data=json.dumps(payload))
+
+    if response.status_code == 201:
+        print('Successfully Updated Control Channel')
+    else:
+        print('Error Updating Control Channel: {}'.format(response.text))
+
 
 if __name__ == '__main__':
     oran = RIC(DOCKER_SOCKET, DOCKER_API_VERSION)
-    ue = UE(ORAN_BASE_URL, MAX_NUMBER_UES)
-
-    ue.ue_create_all()
     
     ts = datetime.now()
-    step_1 = ts + timedelta(seconds=30)
-    step_1_1 = step_1 + timedelta(seconds=5)
-    step_2 = step_1 + timedelta(seconds=30)
-    step_2_1 = step_2 + timedelta(seconds=5)
-    step_3 = step_2 + timedelta(seconds=30)
-    step_3_1 = step_3 + timedelta(seconds=5)
-    step_4 = step_3 + timedelta(seconds=30)
-    step_4_1 = step_4 + timedelta(seconds=5)
-    ts = ts.strftime('%b %d %Y %H:%M:%S')
+    temp_stab = ts
 
-    oran.set_NearRealTimeRIC_scale(10)
+    count_scale_down = 0
+    count_scale_up = 0
 
-    update = False
+    oran.set_NearRealTimeRIC_scale(6)
 
-    with open('oran_sim - {}.csv'.format(ts), 'w') as f:
+    with open('oran_sim - {}.csv'.format(ts.strftime('%b %d %Y %H:%M:%S')), 'w') as f:
         writer = csv.DictWriter(f, fieldnames=COLUMNS)
         writer.writeheader()
-
-        number_ues = 1
-        ue_sim_file = 'ue_sim - {}.csv'.format(ts)
-        ue.ue_sim_start(ue_sim_file, 'w', number_ues)
 
         while True:
             oran_info = oran.get_NearRealTimeRIC_info()
@@ -66,43 +85,38 @@ if __name__ == '__main__':
                     count_perc_cpu_usage = count_perc_cpu_usage + 1                
 
                 writer.writerow(new_row)
-
             
             oran_info['count_perc_cpu_usage'] = count_perc_cpu_usage
             oran_info['avg_perc_cpu_usage'] = avg_perc_cpu_usage
 
             if count_perc_cpu_usage > 0:
                 oran_info['avg_perc_cpu_usage'] = round(avg_perc_cpu_usage / count_perc_cpu_usage, 2)
-                #oran_info['/orion_orion.7.sy10y1917i4zyxe6h5qcz0i0i']['perc_cpu_usage'] = oran_info['/orion_orion.7.sy10y1917i4zyxe6h5qcz0i0i']['perc_cpu_usage'] / count
             
             writer.writerow({'TS': ts_oran, 'ORANAVGCPUUSAGE': oran_info['avg_perc_cpu_usage'], 'ORANSERVICES': oran_info['count_perc_cpu_usage']})
 
-            if (ts >= step_1 and ts <= step_1_1):
-                #oran.set_NearRealTimeRIC_scale(8)
-                number_ues += 2
-                #ue.ue_sim_stop()
-                #time.sleep(0.5)
-                #ue.ue_sim_update(number_ues)
-                print('update')
-                ue.ue_sim_update(number_ues)
-                step_1 += timedelta(seconds=10)
-                step_1_1 += timedelta(seconds=10)
-            '''
-            elif (ts >= step_2 and ts <= step_2_1):
-                #oran.set_NearRealTimeRIC_scale(10)
-            elif (ts >= step_3 and ts <= step_3_1):
-                if not update:
-                    update = True
-                    #number_ues = 20
-                    #ue.ue_sim_stop()
-                    #time.sleep(0.5)
-                    #ue.ue_sim_update(number_ues)
-            elif (ts >= step_4 and ts <= step_4_1):
-                #oran.set_NearRealTimeRIC_scale(12)
-            '''
+            if ts > temp_stab:
+                if oran_info['avg_perc_cpu_usage'] > 1.5:
+                    count_scale_up += 1
+                    count_scale_down = 0
+                elif oran_info['avg_perc_cpu_usage'] < 0.9 and oran_info['avg_perc_cpu_usage'] > 0.1:
+                    count_scale_down += 1
+                    count_scale_up = 0
+                
+            if count_scale_up > 3:
+                oran.set_NearRealTimeRIC_scale(oran.get_NearRealTimeRIC_scale() + 2)
+                count_scale_up = 0
+                temp_stab = ts + timedelta(seconds=5)
+            elif count_scale_down > 3:
+                oran.set_NearRealTimeRIC_scale(oran.get_NearRealTimeRIC_scale() - 2)
+                count_scale_down = 0
+                temp_stab = ts + timedelta(seconds=5)
 
             oran_info = OrderedDict(sorted(oran_info.items()))
 
+            print(json.dumps(oran_info['/mongo_controller.1.bqcp6ca7ytfrz3l3dc2enwxu3']['scale'], indent=4))
+            print(json.dumps(oran_info['avg_perc_cpu_usage'], indent=4))
             print(json.dumps(oran_info, indent=4))
-            print('Number of UEs: {}'.format(number_ues))
+            print('Count up: {} Count down: {}'.format(count_scale_up, count_scale_down))
+            print('ts: {} temp_stab: {}'.format(ts, temp_stab))
+            #print('Number of UEs: {}'.format(number_ues))
             #exit(1)
